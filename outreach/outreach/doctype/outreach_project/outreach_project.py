f5738d2404f84b63a894c3a6f789b58b5816a3f5
AthletiCoder <varunkumar.nagandla@gmail.com>
2025-12-08 18:11:43 +0530

    fixed the outreach project bug

--- outreach/outreach/doctype/outreach_project/outreach_project.py
+++ outreach/outreach/doctype/outreach_project/outreach_project.py
@@ -2,8 +2,4 @@ import frappe
 from frappe.model.document import Document
 
 class OutreachProject(Document):
-    def validate(self):
-        # Ensure participant phones are unique within this project
-        phones = [p.student_phone for p in self.participants]
-        if len(phones) != len(set(phones)):
-            frappe.throw("Duplicate phone found in project participants")
+    pass
